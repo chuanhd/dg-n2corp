8dadeda5be830770ffb9d98c6b1b1dbbafe7eca4
hodanhchuan@gmail.com <hodanhchuan@gmail.com@1c059979-05d6-f143-4524-a40c7ec0ac08>
2013-04-22 18:07:39 +0000

    - Home screen controller

---
+++ trunk/SourceCode/Digitz/Digitz/ViewControllers/UserCell.m
@@ -0,0 +1,29 @@
+//
+//  UserCell.m
+//  Digitz
+//
+//  Created by chuanhd on 4/22/13.
+//  Copyright (c) 2013 N2Corp. All rights reserved.
+//
+
+#import "UserCell.h"
+
+@implementation UserCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
